1d6207a030db2d9e5e4d02327848c40c853428a2
David Rowland <drowland96@gmail.com>
2020-12-08 20:32:57 +0000

    choc:   Added more glue code between choc and juce

--- modules/tracktion_graph/utilities/tracktion_GlueCode.h
+++ modules/tracktion_graph/utilities/tracktion_GlueCode.h
@@ -12,7 +12,7 @@ namespace tracktion_graph
 {
 
 //==============================================================================
-/** Converts a choc::buffer::BufferView to a juce::dsp::AudioBlock<float> */
+/** Converts a choc::buffer::BufferView to a juce::dsp::AudioBlock<float>. */
 template <typename SampleType, template<typename> typename LayoutType>
 juce::dsp::AudioBlock<float> toAudioBlock (const choc::buffer::BufferView<SampleType, LayoutType>& view)
 {
@@ -21,11 +21,55 @@ juce::dsp::AudioBlock<float> toAudioBlock (const choc::buffer::BufferView<Sample
                                          view.size.numFrames);
 }
 
-/** Converts a choc::buffer to a juce::dsp::AudioBlock<float> */
+/** Converts a choc::buffer to a juce::dsp::AudioBlock<float>. */
 template<typename ChocBufferType>
 juce::dsp::AudioBlock<float> toAudioBlock (const ChocBufferType& buffer)
 {
     return toAudioBlock (buffer.getView());
 }
+
+/** Creates a juce::AudioBuffer from a juce::AudioBlock. */
+inline juce::AudioBuffer<float> createAudioBuffer (const juce::dsp::AudioBlock<float>& block)
+{
+    constexpr int maxNumChannels = 128;
+    const int numChannels = std::min (maxNumChannels, (int) block.getNumChannels());
+    float* chans[maxNumChannels] = {};
+
+    for (int i = 0; i < numChannels; ++i)
+        chans[i] = block.getChannelPointer ((size_t) i);
+
+    return juce::AudioBuffer<float> (chans, numChannels, (int) block.getNumSamples());
+}
+
+/** Creates a juce::AudioBuffer from a choc::buffer::BufferView. */
+template <typename SampleType, template<typename> typename LayoutType>
+inline juce::AudioBuffer<float> createAudioBuffer (const choc::buffer::BufferView<SampleType, LayoutType>& view)
+{
+    return createAudioBuffer (toAudioBlock (view));
+}
+
+/** Creates a juce::AudioBuffer from a choc::buffer. */
+template<typename ChocBufferType>
+inline juce::AudioBuffer<float> createAudioBuffer (const ChocBufferType& buffer)
+{
+    return createAudioBuffer (toAudioBlock (buffer));
+}
+
+/** Converts a juce::dsp::AudioBlock<float> to a choc::buffer::BufferView. */
+inline choc::buffer::BufferView<float, choc::buffer::SeparateChannelLayout> toBufferView (const juce::dsp::AudioBlock<float>& block)
+{
+    constexpr int maxNumChannels = 128;
+    const int numChannels = std::min (maxNumChannels, (int) block.getNumChannels());
+    float* chans[maxNumChannels] = {};
+
+    for (int i = 0; i < numChannels; ++i)
+        chans[i] = block.getChannelPointer ((size_t) i);
+
+    return choc::buffer::createChannelArrayView (chans,
+                                                 (choc::buffer::ChannelCount) block.getNumChannels(),
+                                                 (choc::buffer::FrameCount) block.getNumSamples());
+}
+
+
 
 } // namespace tracktion_graph
