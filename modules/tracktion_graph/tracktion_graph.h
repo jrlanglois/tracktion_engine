d86177d6a8f60dcdfd9db3ec8cc9defc02637e2d
David Rowland <drowland96@gmail.com>
2020-12-08 17:30:25 +0000

    choc:   Included choc::buffer

--- modules/tracktion_graph/tracktion_graph.h
+++ modules/tracktion_graph/tracktion_graph.h
@@ -64,6 +64,9 @@
 #include <thread>
 
 //==============================================================================
+#include "../3rd_party/choc/audio/choc_SampleBuffers.h"
+
+//==============================================================================
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_dsp/juce_dsp.h>
 
